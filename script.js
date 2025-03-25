def7fdeb0162b8882a5293b14c94bea759aebea2
d0zxczxc <dayanandrei114@gmail.com>
2025-03-25 10:19:24 +0800

    jkl

---
+++ script.js
@@ -0,0 +1,20 @@
+// Function to update the clock
+function updateClock() {
+    const options = { 
+      timeZone: 'Asia/Manila', 
+      hour: '2-digit', 
+      minute: '2-digit', 
+      second: '2-digit', 
+      hour12: true 
+    };
+  
+    const currentTime = new Date().toLocaleTimeString('en-US', options);
+    document.getElementById("philippines-clock").innerText = currentTime;
+  }
+  
+  // Call the updateClock function every second
+  setInterval(updateClock, 1000);
+  
+  // Initialize the clock
+  updateClock();
+  
