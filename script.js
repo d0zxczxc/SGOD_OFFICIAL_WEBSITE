18d5b3cc6561124d3fbbe12d1e8630d7bff951a0
d0zxczxc <dayanandrei114@gmail.com>
2025-04-10 16:19:05 +0800

    slideshow

--- script.js
+++ script.js
@@ -26,35 +26,40 @@ setInterval(updateClock, 1000);
 updateClock();
 
 
+let slideIndex1 = 0;
+let slideIndex2 = 0;
 
+showSlides1(); // For the first slideshow (Seminar)
+showSlides2(); // For the second slideshow (BERF)
 
-let slideIndex = 0;
-showSlides();
-
-function showSlides() {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  
-  // Hide all slides
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
+function showSlides1() {
+  let slides1 = document.getElementsByClassName("mySlides1");
+  let dots1 = document.getElementsByClassName("dot1");
+  for (let i = 0; i < slides1.length; i++) {
+    slides1[i].style.display = "none";  
   }
-  
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1} // Reset to the first slide if it exceeds the number of slides
-  
-  // Remove active class from all dots
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
+  slideIndex1++;
+  if (slideIndex1 > slides1.length) {slideIndex1 = 1}    
+  for (let i = 0; i < dots1.length; i++) {
+    dots1[i].className = dots1[i].className.replace(" active", "");
   }
-  
-  // Display the current slide and add the active class to the corresponding dot
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
-  
-  // Change the slide every 2 seconds
-  setTimeout(showSlides, 4000); 
+  slides1[slideIndex1 - 1].style.display = "block";  
+  dots1[slideIndex1 - 1].className += " active";
+  setTimeout(showSlides1, 3000); // Change slide every 2 seconds
 }
 
-
+function showSlides2() {
+  let slides2 = document.getElementsByClassName("mySlides2");
+  let dots2 = document.getElementsByClassName("dot2");
+  for (let i = 0; i < slides2.length; i++) {
+    slides2[i].style.display = "none";  
+  }
+  slideIndex2++;
+  if (slideIndex2 > slides2.length) {slideIndex2 = 1}    
+  for (let i = 0; i < dots2.length; i++) {
+    dots2[i].className = dots2[i].className.replace(" active", "");
+  }
+  slides2[slideIndex2 - 1].style.display = "block";  
+  dots2[slideIndex2 - 1].className += " active";
+  setTimeout(showSlides2, 3000); // Change slide every 2 seconds
+}
